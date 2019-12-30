8807a393fe1b089125dec80892f6e4d455964b31
rakesh webtest <rakesh.webtest@gmail.com>
2019-12-30 23:24:28 +0530

    update

--- nestapi/src/meeting/meeting.service.ts
+++ nestapi/src/meeting/meeting.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { MeetingEntity } from './meeting.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, getRepository } from 'typeorm';
 
 @Injectable()
-export class MeetingService {}
+export class MeetingService {
+    constructor(@InjectRepository(MeetingEntity) private readonly meetingRepository: Repository<MeetingEntity>) {
+
+    }
+
+    async getGroups(): Promise<MeetingEntity[]> {
+        return getRepository(MeetingEntity)
+        .createQueryBuilder('m')
+        .select(["m","user.displayName","member.id","member.displayName"])
+        .leftJoin("m.user", 'user')
+        .leftJoin("m.members", 'member')
+        .getMany();
+        // return  this.meetingRepository.find({relations:["user","members"]});
+
+    }
+
+}
