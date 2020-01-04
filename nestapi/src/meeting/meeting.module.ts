5c4ea83b2d070e3a42d22a8f124cfad5c5c775c3
rakesh webtest <rakesh.webtest@gmail.com>
2020-01-04 21:57:31 +0530

    meeting create one

--- nestapi/src/meeting/meeting.module.ts
+++ nestapi/src/meeting/meeting.module.ts
@@ -3,10 +3,12 @@ import { MeetingController } from './meeting.controller';
 import { MeetingService } from './meeting.service';
 import { MeetingEntity } from './meeting.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { GroupEntity } from 'src/group/group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MeetingEntity])],
+  imports: [TypeOrmModule.forFeature([MeetingEntity, UserEntity, GroupEntity])],
   controllers: [MeetingController],
   providers: [MeetingService]
 })
-export class MeetingModule {}
+export class MeetingModule { }
