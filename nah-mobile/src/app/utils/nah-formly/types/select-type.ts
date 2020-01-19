61eebe9ca7d72182fbc2138ee018df4934ecdeb4
rakesh webtest <rakesh.webtest@gmail.com>
2020-01-19 09:34:09 +0530

    update code

--- nah-mobile/src/app/utils/nah-formly/types/select-type.ts
+++ nah-mobile/src/app/utils/nah-formly/types/select-type.ts
@@ -4,7 +4,8 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
     selector: 'formly-field-select',
     template: `
-    <ionic-selectable item-content  [placeholder]="to.placeholder" [itemValueField]="to.itemValueField"
+    <ionic-selectable item-content closeButtonSlot="end" closeButtonText="Close"
+     [placeholder]="to.placeholder" [itemValueField]="to.itemValueField"
     [shouldStoreItemValue]="to.itemValueField"
     [formControl]="formControl" [ionFormlyAttributes]="field"
     [itemTextField]="to.itemTextField" [items]="to.options" [canSearch]="true">
