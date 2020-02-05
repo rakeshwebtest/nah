3f1893e940f340591a00f5252c7c666885ff4ce5
rakeshwebtest <rakesh.webtest@gmail.com>
2020-02-05 11:13:23 +0530

    added meetings comments replys

--- nestapi/src/meeting/meeting-comment-reply.entity.ts
+++ nestapi/src/meeting/meeting-comment-reply.entity.ts
@@ -2,23 +2,19 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, JoinColumn, ManyToMa
 import { BaseEntity } from '../shared/base.entity';
 import { UserEntity } from 'src/user/user.entity';
 import { MeetingEntity } from './meeting.entity';
-import { MeetingCommentReplyEntity } from './meeting-comment-reply.entity';
-@Entity({ name: 'meeting_comments' })
-export class MeetingCommentsEntity extends BaseEntity {
+import { MeetingCommentsEntity } from './meeting-comments.entity';
+@Entity({ name: 'meeting_comment_reply' })
+export class MeetingCommentReplyEntity extends BaseEntity {
 
     @Column({ length: 250, nullable: true })
     comment: string;
 
-    @ManyToOne(type => MeetingEntity, meeting => meeting.comments, { onDelete: 'CASCADE' })
-    meeting: MeetingEntity;
-
-    @OneToMany(type => MeetingCommentReplyEntity, mcr => mcr.comment)
-    replys: MeetingCommentReplyEntity[];
-
     // @Column()
     // userId: number;
     // @Column()
     // userId: number;
+    @ManyToOne(type => MeetingCommentsEntity, mc => mc.replys, { onDelete: 'CASCADE' })
+    meetingComment: MeetingEntity;
 
     @ManyToOne(type => UserEntity, user => user.comments, { onDelete: 'CASCADE' })
     createdBy: UserEntity;
