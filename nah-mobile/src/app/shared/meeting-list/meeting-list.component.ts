c121c5ba7ceb00ddc27b3e4f1b91b1e3851a92eb
rakesh webtest <rakesh.webtest@gmail.com>
2020-03-16 22:06:56 +0530

    updatec

--- nah-mobile/src/app/shared/meeting-list/meeting-list.component.ts
+++ nah-mobile/src/app/shared/meeting-list/meeting-list.component.ts
@@ -129,7 +129,7 @@ export class MeetingListComponent implements OnInit, OnDestroy {
     console.log('eve', eve);
   }
   async doRefresh(event) {
-    this.getMeetings(null, event);
+    // this.getMeetings(null, event);
 
   }
 
