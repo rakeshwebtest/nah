d440f72d61a4f0b4ac2da6d3374764f5b2fea5e7
Raja <palleti1239@gmail.com>
2020-06-06 20:11:27 +0530

    ui for post details and new design for meeting list

--- nah-mobile/src/app/posts/post-detatils/post-detatils.component.ts
+++ nah-mobile/src/app/posts/post-detatils/post-detatils.component.ts
@@ -6,9 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-detatils.component.scss'],
 })
 export class PostDetatilsComponent implements OnInit {
-
+  public postDetails = [];
+  public commentMsg: any;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.postDetails = [
+      {
+        name: 'UZ 16LAB@anties',
+        avatarUrl: 'https://snusercontent.global.ssl.fastly.net/member-profile-full/17/409717_7651259.jpg',
+        createdDate: 'May 31, 2020',
+        imageUrl: 'https://static.planetminecraft.com/files/resource_media/screenshot/1506/nah8616087.jpg'
+      }
+    ];
+  }
 
 }
