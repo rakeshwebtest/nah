24effe916e3395c5aea14b233ea2c59aa210783c
rakesh webtest <rakesh.webtest@gmail.com>
2020-06-26 11:24:10 -0700

    update code

--- nah-mobile/src/app/sign-in/choose-user-groups/choose-user-groups.component.ts
+++ nah-mobile/src/app/sign-in/choose-user-groups/choose-user-groups.component.ts
@@ -74,7 +74,7 @@ export class ChooseUserGroupsComponent implements OnInit {
           this.nativeStorage.set('USER_INFO', user);
         });
         this.loadingService.hide();
-        this.router.navigate(['/dashboard/meeting/all']);
+        this.router.navigate(['/dashboard']);
       });
     }
   }
