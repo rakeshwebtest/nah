ceaf39e87019b247998c5402688b94bacb5a6e49
Mohan <palaekirimohanbabu@gmail.com>
2020-06-28 20:02:09 +0530

    added entities in the post controller

--- nestapi/src/posts/posts.module.ts
+++ nestapi/src/posts/posts.module.ts
@@ -4,11 +4,17 @@ import { AgendaController } from './agenda/agenda.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgendaEntity } from './agenda.entity';
 import { UserEntity } from './../user/user.entity';
+import { PostEntity } from './post.entity';
+import { PostPhotosEntity } from './post-photos.entity';
+import { PostVideosEntity } from './post-videos.entity';
+import { PostCommentsEntity } from './post-comments.entity';
+import { PostCommentReplyEntity } from './post-comment-reply.entity';
+import { AgendaTopicsEntity } from './agenda-topics.entity';
 import { AgendaService } from './agenda/agenda.service';
 import { PostService } from './posts/post.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AgendaEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([AgendaEntity, UserEntity, PostEntity, PostPhotosEntity, PostVideosEntity, PostCommentsEntity, PostCommentReplyEntity, AgendaTopicsEntity])],
   controllers: [PostsController, AgendaController],
   providers:[AgendaService, PostService]
 })
