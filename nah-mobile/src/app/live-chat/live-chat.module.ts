d6d735ba3fec4b939ab7b041487eee3e79ce3f57
rakeshwebtest <rakesh.webtest@gmail.com>
2020-07-14 21:41:09 +0530

    update code

--- nah-mobile/src/app/live-chat/live-chat.module.ts
+++ nah-mobile/src/app/live-chat/live-chat.module.ts
@@ -11,7 +11,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
 import { ChatService } from './chat.service';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: environment.apiUrl, options: {} };
 
 @NgModule({
   imports: [
