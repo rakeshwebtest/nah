d2751f72324d1a2d205970be6e79740b41b77ac3
rakeshwebtest <rakesh.webtest@gmail.com>
2020-08-12 19:11:29 +0530

    fixed Db issue

--- nestapi/src/config.ts
+++ nestapi/src/config.ts
@@ -1,16 +1,7 @@
 export interface AppConfig {
     SECRET: string;
     SERVERBASEPATH: string;
-    DB?: {
-        type?: string;
-        host?: any;
-        port?: any;
-        username?: any;
-        password?: any;
-        database?: any;
-        entities?: any;
-        synchanyronize?: any;
-    };
+    DB: any;
     SERVER_PORT: number;
     S3: {
         AWS_ACCESS_KEY_ID: string;
