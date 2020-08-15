aa5930f641f0c675986350131db04b58e87b3cf9
rakeshwebtest <rakesh.webtest@gmail.com>
2020-08-16 02:43:15 +0530

    update code

--- nestapi/src/app.controller.ts
+++ nestapi/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Res, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import * as sharp from 'sharp';
 import { ImageResizeOptionsDto } from './assets/asset.dto';
+import { AssetService } from './assets/asset.service';
 
 @ApiTags('App')
 @Controller()
@@ -14,39 +15,18 @@ export class AppController {
   }
   @Get('uploads/:imgpath')
   async seeUploadedFile(@Param('imgpath') image, @Res() res, @Query() query: ImageResizeOptionsDto) {
-    // try {
-    //   if (query.w || query.h) {
-    //     let width: any;
-    //     let height: any;
-    //     if (query.w) {
-    //       width = parseInt(query.w, null);
-    //     }
-    //     if (query.h) {
-    //       height = parseInt(query.h, null);
-    //     }
-    //     const imagePath = await sharp('./uploads/' + image)
-    //       .rotate()
-    //       .resize(width, height, query.fit)
-    //       .toBuffer()
-    //       .then(data => {
-    //         console.log('data', data);
-    //         return data;
-    //       })
-    //       .catch(err => {
-    //         return err;
-    //       });
+    let width: any;
+    let height: any;
+    if (query.w) {
+      width = parseInt(query.w, null);
+    }
+    if (query.h) {
+      height = parseInt(query.h, null);
+    }
+    const imagePath = await sharp('./uploads/' + image)
+      .resize(width, height, query.fit)
+      .toBuffer();
+    return res.status(200).end(imagePath);
 
-    //     return res.end(imagePath, 'binary');
-    //   } else {
-    //     const file = res.sendFile(image, { root: './uploads' });
-    //     return file;
-    //   }
-    // } catch (error) {
-    //   const file = res.sendFile(image, { root: './uploads' });
-    //   return file;
-    // }
-
-    const file = res.sendFile(image, { root: './uploads' });
-    return file;
   }
 }
