841b2aa2f688135dc36d6a2798d36d107a6ddc45
rakeshwebtest <rakesh.webtest@gmail.com>
2020-08-16 01:13:51 +0530

    update code s

--- nestapi/src/app.controller.ts
+++ nestapi/src/app.controller.ts
@@ -14,36 +14,39 @@ export class AppController {
   }
   @Get('uploads/:imgpath')
   async seeUploadedFile(@Param('imgpath') image, @Res() res, @Query() query: ImageResizeOptionsDto) {
-    try {
-      if (query.w || query.h) {
-        let width: any;
-        let height: any;
-        if (query.w) {
-          width = parseInt(query.w, null);
-        }
-        if (query.h) {
-          height = parseInt(query.h, null);
-        }
-        const imagePath = await sharp('./uploads/' + image)
-          .rotate()
-          .resize(width, height, query.fit)
-          .toBuffer()
-          .then(data => {
-            console.log('data', data);
-            return data;
-          })
-          .catch(err => {
-            return err;
-          });
+    // try {
+    //   if (query.w || query.h) {
+    //     let width: any;
+    //     let height: any;
+    //     if (query.w) {
+    //       width = parseInt(query.w, null);
+    //     }
+    //     if (query.h) {
+    //       height = parseInt(query.h, null);
+    //     }
+    //     const imagePath = await sharp('./uploads/' + image)
+    //       .rotate()
+    //       .resize(width, height, query.fit)
+    //       .toBuffer()
+    //       .then(data => {
+    //         console.log('data', data);
+    //         return data;
+    //       })
+    //       .catch(err => {
+    //         return err;
+    //       });
 
-        return res.end(imagePath, 'binary');
-      } else {
-        const file = res.sendFile(image, { root: './uploads' });
-        return file;
-      }
-    } catch (error) {
-      const file = res.sendFile(image, { root: './uploads' });
-      return file;
-    }
+    //     return res.end(imagePath, 'binary');
+    //   } else {
+    //     const file = res.sendFile(image, { root: './uploads' });
+    //     return file;
+    //   }
+    // } catch (error) {
+    //   const file = res.sendFile(image, { root: './uploads' });
+    //   return file;
+    // }
+
+    const file = res.sendFile(image, { root: './uploads' });
+    return file;
   }
 }
