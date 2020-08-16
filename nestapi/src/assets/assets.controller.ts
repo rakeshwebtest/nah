ce589dc693d5e9acac0d21b92b99b2b30f831eb6
rakeshwebtest <rakesh.webtest@gmail.com>
2020-08-16 14:02:34 +0530

    fixed image

--- nestapi/src/assets/assets.controller.ts
+++ nestapi/src/assets/assets.controller.ts
@@ -28,11 +28,12 @@ export const imageFileFilter = (req, file, callback) => {
 export const editFileName = (req, file, callback) => {
     const name = file.originalname.split('.')[0];
     const fileExtName = extname(file.originalname);
-    const randomName = Array(4)
+    const timeStapm = new Date().getTime();
+    const randomName = Array(32)
         .fill(null)
         .map(() => Math.round(Math.random() * 16).toString(16))
         .join('');
-    callback(null, `${name}-${randomName}${fileExtName}`);
+    callback(null, `post-${timeStapm}-${randomName}${fileExtName}`);
 };
 @ApiTags('Asset')
 @Controller('asset')
@@ -72,7 +73,7 @@ export class AssestsController {
                 destination: './uploads',
                 filename: editFileName,
             }),
-            fileFilter: imageFileFilter,
+            fileFilter: imageFileFilter
         }),
     )
     async uploadMultipleFiles(@UploadedFiles() files, @Body() data: ImagesDto) {
