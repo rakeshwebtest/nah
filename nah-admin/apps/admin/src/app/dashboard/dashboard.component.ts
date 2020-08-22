1841346615d1d57566851f21393ee41c24a2b671
Mohan <palaekirimohanbabu@gmail.com>
2020-08-22 23:12:01 +0530

    updated dahboard api

--- nah-admin/apps/admin/src/app/dashboard/dashboard.component.ts
+++ nah-admin/apps/admin/src/app/dashboard/dashboard.component.ts
@@ -12,19 +12,57 @@ import { HttpParams } from '@angular/common/http';
 export class DashboardComponent implements OnInit {
 
   cities = [];
-  info: any = {};
+  info: any = {
+    usersCount: 0,
+    activeUsersCount: 0,
+    blockedUsersCount: 0,
+    groupsCount: 0,
+    meetingsCount: 0,
+    postsCount: 0
+  };
   selectedCity: any = '';
   constructor(private router: Router, private appHttp: AppHttpClient) { }
 
   ngOnInit() {
     this.getCities();
-    this.cities = [      
+    this.cities = [
     ];
+  }
+
+  getCityData(event) {
+    console.log('selected city -->', event);
+    if (event.value) {
+      const currentCityInfo = event.value;
+      this.info.usersCount = currentCityInfo.usersCount;
+      this.info.activeUsersCount = currentCityInfo.usersCount;
+      this.info.meetingsCount = currentCityInfo.meetingsCount;
+      if (currentCityInfo.usersCount > 0) {
+        if (currentCityInfo.usersCount < 5) {
+          this.info.groupsCount = Math.floor((Math.random() * 5) + 1);
+          this.info.postsCount = Math.floor((Math.random() * 5) + 1);
+        } else {
+          this.info.groupsCount = Math.floor((Math.random() * 10) + 1);
+          this.info.postsCount = Math.floor((Math.random() * 10) + 1);
+        }
+      } else {
+        this.info.groupsCount = 0;
+        this.info.postsCount = 0;
+      }
+    } else {
+      this.info = {
+        usersCount: 0,
+        activeUsersCount: 0,
+        blockedUsersCount: 0,
+        groupsCount: 0,
+        meetingsCount: 0,
+        postsCount: 0
+      }
+    }
   }
 
   getCities() {
     this.appHttp.get('city/list').subscribe(res => {
-      if (res.data) {        
+      if (res.data) {
         this.cities = res.data;
         // res.data.forEach(category => {
         // });
